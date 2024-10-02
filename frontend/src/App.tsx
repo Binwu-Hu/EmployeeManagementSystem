5e50c74ab23adba6b0ce105413916dc22d3e643d
Michelle Hu <binwuhu@outlook.com>
2024-10-02 16:23:11 -0400

    Add ErrorPage to protect private routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import Signup from './pages/auth/Signup';
+import ErrorPage from './pages/error/ErrorPage';
 import SendRegistrationLink from './pages/protected/SendRegistrationLink';
 import PrivateRoute from './components/PrivateRoute';
 import AppLayout from './components/Layout/Layout';
@@ -12,11 +13,12 @@ const App = () => {
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/signup/:token' element={<Signup />} />
+          <Route path='/error' element={<ErrorPage />} />
           <Route 
             path='/send-registration-link' 
             element={
-              <PrivateRoute>
-                <SendRegistrationLink />
+              <PrivateRoute  requiredRole="HR">
+                <SendRegistrationLink/>
               </PrivateRoute>
             }
           />
