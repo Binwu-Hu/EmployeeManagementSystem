bb574c4abdc0ec729262b95fe68d93aca0be9589
Zhiyuan Jiang <zhiyuanj@andrew.cmu.edu>
2024-10-04 06:27:20 -0400

    persist redux state using redux-persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,18 @@
-import './index.css'
+import './index.css';
 
-import App from './App.tsx'
-import { Provider } from 'react-redux'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { store } from './app/store'
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './app/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
-)
+);
