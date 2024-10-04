4dac2a47eb9b7ca0c91731d08e03ac6f115bc8d1
Michelle Hu <binwuhu@outlook.com>
2024-10-04 17:53:42 -0400

    Format the Visa Status Management page for Employee

---
+++ frontend/src/components/VisaStatus/I983FormSection.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { uploadVisaDocument } from '../../features/visaStatus/visaStatusSlice';
+import { RootState } from '../../app/store';
+import { Upload, Button, message, Card } from 'antd';
+
+const I983FormSection = ({ employeeId }: { employeeId: string }) => {
+  const dispatch = useDispatch();
+  const { visaStatus } = useSelector((state: RootState) => state.visaStatus);
+  const [files, setFiles] = React.useState([]);
+
+  const handleFileChange = (info: any) => {
+    setFiles(info.fileList);
+  };
+
+  const handleSubmit = () => {
+    if (!files.length) {
+      message.error('Please upload a file!');
+      return;
+    }
+
+    const fileList = files.map(file => file.originFileObj).filter(Boolean);
+    dispatch(uploadVisaDocument({ employeeId, fileType: 'i983Form', files: fileList }))
+      .then(() => {
+        message.success('I-983 Form upload successful');
+      })
+      .catch((error) => {
+        message.error(error.message);
+      });
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
+      <Card
+        title="I-983 Form"
+        bordered={false}
+        style={{ width: 400, textAlign: 'center' }}
+      >
+        {visaStatus?.i983Form?.status !== 'Approved' ? (
+          <>
+            <p>Waiting for HR to approve your I-983 Form</p>
+            <Upload multiple onChange={handleFileChange} fileList={files}>
+              <Button>Select File</Button>
+            </Upload>
+            <Button type="primary" onClick={handleSubmit} className="mt-3">
+              Upload I-983 Form
+            </Button>
+          </>
+        ) : (
+          <p>Your I-983 Form is approved.</p>
+        )}
+      </Card>
+    </div>
+  );
+};
+
+export default I983FormSection;
--- frontend/src/pages/protected/VisaStatus.tsx
+++ frontend/src/pages/protected/VisaStatus.tsx
@@ -1,15 +1,20 @@
-import { AppDispatch, RootState } from '../../app/store';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
-import VisaStatusForm from '../../components/VisaStatus/VisaStatusForm';
-import VisaStatusList from '../../components/VisaStatus/VisaStatusList';
+import { RootState } from '../../app/store';
 import { fetchEmployeeByUserId } from '../../features/employee/employeeSlice';
-import { useEffect } from 'react';
+import OPTReceiptSection from '../../components/VisaStatus/OPTReceiptSection';
+import OPTEADSection from '../../components/VisaStatus/OPTEADSection';
+import I983FormSection from '../../components/VisaStatus/I983FormSection';
+import I20FormSection from '../../components/VisaStatus/I20FormSection';
+import { Layout, Menu } from 'antd';
+
+const { Sider, Content } = Layout;
 
 const VisaStatusPage = () => {
-  const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch();
   const { user } = useSelector((state: RootState) => state.user);
   const { employee, loading, error } = useSelector((state: RootState) => state.employee);
+  const [selectedKey, setSelectedKey] = useState('optReceipt');
 
   useEffect(() => {
     if (user?.email) {
@@ -20,18 +25,49 @@ const VisaStatusPage = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
+  const handleMenuClick = (key: string) => {
+    setSelectedKey(key);
+  };
+
+  const renderSection = () => {
+    switch (selectedKey) {
+      case 'optReceipt':
+        return <OPTReceiptSection employeeId={employee._id} />;
+      case 'optEAD':
+        return <OPTEADSection employeeId={employee._id} />;
+      case 'i983Form':
+        return <I983FormSection employeeId={employee._id} />;
+      case 'i20Form':
+        return <I20FormSection employeeId={employee._id} />;
+      default:
+        return <p>Select a section</p>;
+    }
+  };
+
   return (
-    <div className="container mx-auto my-5">
-      <h1 className="text-2xl font-bold text-center mb-4">Visa Status Management</h1>
-      {employee ? (
-        <>
-          <VisaStatusForm employeeId={employee._id} />
-          <VisaStatusList employeeId={employee._id} />
-        </>
-      ) : (
-        <p className="text-center text-red-500">You do not have access to this page.</p>
-      )}
-    </div>
+    <Layout className='min-h-screen'>
+      <Sider width={200} className='bg-gray-800 text-white h-screen sticky top-0'>
+        <Menu
+          mode='inline'
+          theme='dark'
+          className='h-full'
+          selectedKeys={[selectedKey]}
+          onClick={(e) => handleMenuClick(e.key)}
+        >
+          <Menu.Item key='optReceipt'>OPT Receipt</Menu.Item>
+          <Menu.Item key='optEAD'>OPT EAD</Menu.Item>
+          <Menu.Item key='i983Form'>I-983 Form</Menu.Item>
+          <Menu.Item key='i20Form'>I-20 Form</Menu.Item>
+        </Menu>
+      </Sider>
+
+      <Layout className='bg-gray-50'>
+        <Content className='p-6'>
+          <h1 className='text-2xl font-bold mb-4'>Visa Status Management</h1>
+          {employee && renderSection()}
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
