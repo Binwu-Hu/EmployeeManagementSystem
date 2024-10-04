9e932ea10b512cb6e8b9aee8714163e37c9555f5
Michelle Hu <binwuhu@outlook.com>
2024-10-03 23:22:18 -0400

    Add first name and last name in token models

--- backend/models/registrationTokenModel.js
+++ backend/models/registrationTokenModel.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const registrationTokenSchema = new mongoose.Schema({
   token: { type: String, required: true, unique: true },
   email: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   expiresAt: { type: Date, required: true },
 }, { timestamps: true });
 
