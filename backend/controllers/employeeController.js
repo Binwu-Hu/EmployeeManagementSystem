c57592b47c0aa4f5915d79cef87630e8b50473d8
Zhengyu Yang <yangzy0105@gmail.com>
2024-10-07 11:58:48 -0700

    clear workAuthorization files if visaType has changed from F1

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -23,45 +23,86 @@ export const getEmployeeByUserId = async (req, res) => {
   }
 };
 
+// export const updateEmployee = async (req, res) => {
+//   const userId = req.params.id;
+
+//   try {
+//     const updatedEmployee = await Employee.findOneAndUpdate(
+//       { userId },
+//       { $set: req.body },
+//       { new: true, runValidators: true } // Return updated employee, run validation
+//     );
+
+//     if (!updatedEmployee) {
+//       return res.status(404).json({ message: 'Employee not found' });
+//     }
+
+//     const visaType = updatedEmployee?.workAuthorization?.visaType;
+
+//     const files = updatedEmployee?.workAuthorization?.files;
+
+//     try {
+//       if (files) {
+//         await updateVisaStatus(updatedEmployee._id, visaType, files);
+//       } else {
+//         await updateVisaStatus(updatedEmployee._id, visaType);
+//       }
+//     } catch (error) {
+//       console.error('Error updating visa status:', error.message);
+//       return res.status(500).json({
+//         message: 'Error updating visa status',
+//         error: error.message,
+//       });
+//     }
+
+//     res.status(200).json(updatedEmployee);
+//   } catch (error) {
+//     res
+//       .status(500)
+//       .json({ message: 'Error updating employee', error: error.message });
+//   }
+// };
+
 export const updateEmployee = async (req, res) => {
   const userId = req.params.id;
-//   console.log("userId:" ,userId)
-  console.log("req.body:" ,req.body)
+
   try {
-    const updatedEmployee = await Employee.findOneAndUpdate(
+    let updatedEmployee = await Employee.findOneAndUpdate(
       { userId },
       { $set: req.body },
       { new: true, runValidators: true } // Return updated employee, run validation
     );
-    console.log('here0');
 
     if (!updatedEmployee) {
       return res.status(404).json({ message: 'Employee not found' });
     }
-    console.log("employee controller updatedEmployee:", updatedEmployee);
+
+    // Check if the visaType is not 'F1', and if so, remove the files field
+    if (updatedEmployee?.workAuthorization?.visaType !== 'F1') {
+      updatedEmployee = await Employee.findOneAndUpdate(
+        { userId },
+        { $unset: { 'workAuthorization.files': 1 } },
+        { new: true } // Return the updated document
+      );
+    }
+
     const visaType = updatedEmployee?.workAuthorization?.visaType;
-    console.log('employee controller visaType:', visaType);
+
     const files = updatedEmployee?.workAuthorization?.files;
-    console.log('employee controller files:', files);
+
     try {
-        console.log('here3');
+      // Update visa status based on the existence of files
       if (files) {
-        console.log('here4');
         await updateVisaStatus(updatedEmployee._id, visaType, files);
-        console.log('save succcessfully in employee controller');
       } else {
-        console.log('here5');
         await updateVisaStatus(updatedEmployee._id, visaType);
-        console.log('save succcessfully without files in employee controller');
       }
     } catch (error) {
       console.error('Error updating visa status:', error.message);
-      return res
-        .status(500)
-        .json({
-          message: 'Error updating visa status',
-          error: error.message,
-        });
+      return res.status(500).json({
+        message: 'Error updating visa status',
+        error: error.message,
+      });
     }
 
     res.status(200).json(updatedEmployee);
